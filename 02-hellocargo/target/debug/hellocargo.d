f2cf2475b26237ec0fd564672c02269566749b39
hackerhgl <hackerhgl@gmail.com>
2022-06-12 06:30:39 +0500

    Hellow world

---
+++ 02-hellocargo/target/debug/hellocargo.d
@@ -0,0 +1,1 @@
+/Users/hackerhgl/Documents/programming/opensource/learn-rust/02-hellocargo/target/debug/hellocargo: /Users/hackerhgl/Documents/programming/opensource/learn-rust/02-hellocargo/src/main.rs
